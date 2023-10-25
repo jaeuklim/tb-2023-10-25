cddc0a8fd61d31dce67723ed5104277a32314168
jaeuklim <jeuk007@skuniv.ac.kr>
2023-10-25 15:40:40 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,13 +6,20 @@ public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명령) ");
+        while(true) {
+            System.out.print("명령) ");
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
-        // 위 라인은 아래 라인과 같은 뜻
-        // System.out.print("입력하신 명령 : " + cmd + "\n");
+            if(cmd .equals("종료"))
+                break;
+
+            System.out.printf("입력하신 명령 : %s\n", cmd);
+            // 위 라인은 아래 라인과 같은 뜻
+            // System.out.print("입력하신 명령 : " + cmd + "\n");
+        }
+
+
     }
 }
